3a4a7be1ac93866698cc1ea667b69888ecad3fae
Fermedina <fermed28@gmai.com>
2017-12-09 18:26:16 -0500

    agrega funcionalidad de buscador

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,20 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+     @expenses = Expense.order("date DESC")    
+
+    
+     
+     unless params[:concept] && params[:concept].nil?     	
+     	@expenses=@expenses.where("concept like :query",query: "%#{params[:concept]}%")
+     end 
+
+      puts '*'*50
+     puts params 
+    
+     if params[:category_id] !="" && params[:category_id] !=nil  
+     puts  	"entro aqui #{params[:category_id].nil?}"
+      @expenses= @expenses.where(category_id: params[:category_id])     
+     end   
+
   end
 end
